da3bed5f0c9c214271b538a0376fcce982909d8f
Ryanna Luo <ryanna668@gmail.com>
2022-01-24 09:07:45 -0500

    Added enemy class

---
+++ app/src/main/java/com/group2/androidgame/Enemy.java
@@ -0,0 +1,15 @@
+package com.group2.androidgame;
+
+import androidx.constraintlayout.core.widgets.Rectangle;
+
+public class Enemy extends Rectangle {
+    static final int W = 80;
+    static final int H = 80;
+
+    Enemy(int x, int y, int w, int h){
+        this.x =x;
+        this.y = y;
+        width = W;
+        height = H;
+    }
+}
